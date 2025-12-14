645e3f59bf9cb0111c3983e4f5abf9151d933419
c6zrr24fbk-hub <c6zrr24fbk@privaterelay.appleid.com>
2025-12-14 21:00:36 +0300

    Revert "fix: исправление ошибок в daysteps и spentcalories"
    This reverts commit b71e4b4a35b6df3740b3ce53fbe840d8ec4b3126.

--- internal/spentcalories/spentcalories.go
+++ internal/spentcalories/spentcalories.go
@@ -1,10 +1,6 @@
 package spentcalories
 
 import (
-	"fmt"
-	"log"
-	"strconv"
-	"strings"
 	"time"
 )
 
@@ -18,129 +14,25 @@ const (
 )
 
 func parseTraining(data string) (int, string, time.Duration, error) {
-	parts := strings.Split(data, ",")
-	if len(parts) != 3 {
-		return 0, "", 0, fmt.Errorf("неверный формат данных")
-	}
-
-	steps, err := strconv.Atoi(parts[0])
-	if err != nil {
-		return 0, "", 0, fmt.Errorf("ошибка парсинга шагов: %v", err)
-	}
-
-	if steps <= 0 {
-		return 0, "", 0, fmt.Errorf("количество шагов должно быть положительным")
-	}
-
-	activityType := parts[1]
-	duration, err := time.ParseDuration(parts[2])
-	if err != nil {
-		return 0, "", 0, fmt.Errorf("ошибка парсинга времени: %v", err)
-	}
-
-	if duration <= 0 {
-		return 0, "", 0, fmt.Errorf("продолжительность должна быть положительной")
-	}
-
-	return steps, activityType, duration, nil
+	// TODO: реализовать функцию
 }
 
 func distance(steps int, height float64) float64 {
-	stepLength := height * stepLengthCoefficient
-	distanceMeters := float64(steps) * stepLength
-	return distanceMeters / mInKm
+	// TODO: реализовать функцию
 }
 
 func meanSpeed(steps int, height float64, duration time.Duration) float64 {
-	if duration <= 0 {
-		return 0
-	}
-	dist := distance(steps, height)
-	hours := duration.Hours()
-	if hours == 0 {
-		return 0
-	}
-	return dist / hours
+	// TODO: реализовать функцию
 }
 
 func TrainingInfo(data string, weight, height float64) (string, error) {
-	steps, activityType, duration, err := parseTraining(data)
-	if err != nil {
-		log.Println(err)
-		return "", err
-	}
-	var calories float64
-	var dist float64
-	var speed float64
-	switch strings.ToLower(activityType) {
-	case "бег", "running":
-		calories, err = RunningSpentCalories(steps, weight, height, duration)
-		if err != nil {
-			log.Println(err)
-			return "", err
-		}
-		dist = distance(steps, height)
-		speed = meanSpeed(steps, height, duration)
-	case "ходьба", "walking":
-		calories, err = WalkingSpentCalories(steps, weight, height, duration)
-		if err != nil {
-			log.Println(err)
-			return "", err
-		}
-		dist = distance(steps, height)
-		speed = meanSpeed(steps, height, duration)
-	default:
-		return "", fmt.Errorf("неизвестный тип тренировки")
-	}
-	info := fmt.Sprintf("Тип тренировки: %s\n", activityType)
-	info += fmt.Sprintf("Длительность: %.2f ч.\n", duration.Hours())
-	info += fmt.Sprintf("Дистанция: %.2f км.\n", dist)
-	info += fmt.Sprintf("Скорость: %.2f км/ч\n", speed)
-	info += fmt.Sprintf("Сожгли калорий: %.2f\n", calories)
-	return info, nil
+	// TODO: реализовать функцию
 }
 
 func RunningSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	if steps <= 0 {
-		return 0, fmt.Errorf("количество шагов должно быть положительным")
-	}
-	if weight <= 0 {
-		return 0, fmt.Errorf("вес должен быть положительным")
-	}
-	if height <= 0 {
-		return 0, fmt.Errorf("рост должен быть положительным")
-	}
-	if duration <= 0 {
-		return 0, fmt.Errorf("продолжительность должна быть положительной")
-	}
-	speed := meanSpeed(steps, height, duration)
-	if speed == 0 {
-		return 0, fmt.Errorf("невозможно вычислить скорость")
-	}
-	durationMinutes := duration.Minutes()
-	calories := (weight * speed * durationMinutes) / minInH
-	return calories, nil
+	// TODO: реализовать функцию
 }
 
 func WalkingSpentCalories(steps int, weight, height float64, duration time.Duration) (float64, error) {
-	if steps <= 0 {
-		return 0, fmt.Errorf("количество шагов должно быть положительным")
-	}
-	if weight <= 0 {
-		return 0, fmt.Errorf("вес должен быть положительным")
-	}
-	if height <= 0 {
-		return 0, fmt.Errorf("рост должен быть положительным")
-	}
-	if duration <= 0 {
-		return 0, fmt.Errorf("продолжительность должна быть положительной")
-	}
-	speed := meanSpeed(steps, height, duration)
-	if speed == 0 {
-		return 0, fmt.Errorf("невозможно вычислить скорость")
-	}
-	durationMinutes := duration.Minutes()
-	calories := (weight * speed * durationMinutes) / minInH
-	calories *= walkingCaloriesCoefficient
-	return calories, nil
+	// TODO: реализовать функцию
 }
